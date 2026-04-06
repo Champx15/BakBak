8df9fd62e218c7751bde4f42b1262759761af862
champx15 <munawarxpower@gmail.com>
2026-04-06 17:12:43 +0530

    fix: removed old imports

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter,createRoutesFromElements, RouterProvider,Route } from "react-router"
 import Landing from "./pages/Landing"
-import Chat from "./pages/Chat"
 import Test from "./pages/Test"
 import TestMobile from "./pages/TestMobile"
 import { useEffect,useState } from "react"
